142243c5d830668b21a61212ff94b25183e72088
hybridreaver1065 <42978468+hybridreaver1065@users.noreply.github.com>
2018-10-12 14:26:43 -0400

    Add files via upload

--- Eurydice/src/TargetDice.java
+++ Eurydice/src/TargetDice.java
@@ -4,6 +4,14 @@ public class TargetDice {
 	public static void main(String[] args) {
 		
 		Scanner in= new Scanner(System.in);
+		
+		Dice die1 = new Dice();
+		Dice die2 = new Dice();
+		
+		boolean gamewon = false;
+		//Biggest Loop
+		while (!gamewon) {
+		
 		System.out.print("Please enter in an integer");
 		
 		while (!in.hasNextInt()) {
@@ -11,7 +19,22 @@ public class TargetDice {
 		System.out.print("Please enter in an integer");
 		}
 		
-	in.nextInt();
+		
+		
+		int target =in.nextInt();
+		int face1 = die1.roll();
+		int face2 = die2.roll();
+		if (face1+face2 != target) {
+			System.out.print("You rolled a" + face1+ " and a "+ face2+" so you did not roll your target");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 		System.out.print("Bourgeoisie");
 	}
 }
